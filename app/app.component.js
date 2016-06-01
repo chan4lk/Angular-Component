03375e37cdd5aa4ea9434311c9e377d3f5c12a8b
Chandima Ranaweera <Chandima Ranaweera>
2016-06-01 23:20:13 +0530

    init

---
+++ app/app.component.js
@@ -0,0 +1,41 @@
+(function () {
+    function fetchMovies($http) {
+        return $http.get("/movies.json").then(function (response) {
+            return response.data;
+        });
+    }
+    
+    function Controller($http) {
+            var vm = this;
+            
+            vm.$onInit = function () {
+               
+                fetchMovies($http).then(function (data) {
+                    vm.movies = data;
+                });
+            }
+            
+            vm.upRating = function (movie) {
+                if(movie.rating < 5){
+                    movie.rating += 1;
+                }
+            }
+            
+            vm.downRating = function (movie) {
+                if(movie.rating > 0){
+                    movie.rating -= 1;
+                }
+            }
+        }
+        
+    angular.module('app')
+    .component('movieList', {
+         bindings:{
+            myname: '<?'
+        },
+        templateUrl: "/app/HTML/movielist.html",
+        controllerAs: "vm",
+        controller: ['$http', Controller]       
+    });
+    
+})();
